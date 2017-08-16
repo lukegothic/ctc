b3a81b5f8a539ec401ffc154c44e5be1f0e3ef30
Iván <lukegothic@gmail.com>
2017-08-16 06:27:01 +0200

    Limpieza de lo no necesario

--- server/functions.php
+++
@@ -1,6 +0,0 @@
-<?php
-function getElapsedTime($date, $inms = false) {
-	$elapsedSeconds = date_create()->getTimestamp() - date_create($date)->getTimestamp();
-	return $elapsedSeconds * ($inms ? 1000 : 1);
-}
-?>
--- server/locations.php
+++
@@ -1,8 +0,0 @@
-<?php
-header('Access-Control-Allow-Origin: *');
-
-require "db.php";
-
-echo json_encode($db->query("SELECT id, ST_AsGeoJson(geom) geom FROM locations")->FetchAll(PDO::FETCH_ASSOC));
-
-?>
